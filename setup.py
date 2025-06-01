ce57556e6a8aa1c89a83dc24cf364cd3341bf2e1
Wesley Alexander Houser <houser2388@gmail.com>
2025-06-01 08:07:15 -0400

    Bump version to 1.2.0 and update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="stocksim",
-    version="1.1.0",
+    version="1.2.0",
     description="Monte Carlo Stock/Crypto Price Simulation Tool",
     long_description=long_description,
     long_description_content_type="text/markdown",
