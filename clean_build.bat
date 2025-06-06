a05072b10e6f45bc7ad7059dde4b8f6022e8f465
Wesley Alexander Houser <houser2388@gmail.com>
2025-06-06 12:54:20 -0400

    Release v2.1.1: version bump and packaging updates

--- clean_build.bat
+++ clean_build.bat
@@ -1,6 +1,25 @@
 @echo off
-rmdir /s /q build
-rmdir /s /q dist
-for /d %%i in (*.egg-info) do rmdir /s /q "%%i"
+REM Remove build directory if it exists
+if exist build (
+    echo Deleting build directory...
+    rmdir /s /q build
+)
+
+REM Remove dist directory if it exists
+if exist dist (
+    echo Deleting dist directory...
+    rmdir /s /q dist
+)
+
+REM Remove all .egg-info directories in the current folder
+for /d %%i in (*.egg-info) do (
+    echo Deleting %%i ...
+    rmdir /s /q "%%i"
+)
+
+echo.
+echo Building source distribution and wheel...
 python -m build --sdist
 python -m build --wheel
+
+pause
